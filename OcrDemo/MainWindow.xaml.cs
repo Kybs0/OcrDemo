7d8c416479d6df4f131ccabe9c1f3bdd50be477d
yudong <1016724958@qq.com>
2021-06-15 20:17:31 +0800

    添加文件输出

--- OcrDemo/MainWindow.xaml.cs
+++ OcrDemo/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using IronOcr;
 using Microsoft.Win32;
@@ -15,7 +18,7 @@ namespace OcrDemo
             InitializeComponent();
         }
 
-        private void SelectFileButton_OnClick(object sender, RoutedEventArgs e)
+        private async void SelectFileButton_OnClick(object sender, RoutedEventArgs e)
         {
             var openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "图片|*.jpg;*.png;*.gif;*.jpeg;*.bmp|pdf文件|*.pdf";
@@ -24,12 +27,12 @@ namespace OcrDemo
             {
                 var fileName = openFileDialog.FileName;
                 FileNameTextBlock.Text = fileName;
-                var orcText = OcrFile(fileName);
+                var orcText =await OcrFile(fileName);
                 OutputTextBox.Text = orcText;
             }
         }
 
-        private string OcrFile(string fileName)
+        private async Task<string> OcrFile(string fileName)
         {
             //https://ironsoftware.com/csharp/ocr/
             var ironTeseract = new IronTesseract();
@@ -38,9 +41,18 @@ namespace OcrDemo
             ironTeseract.Language = OcrLanguage.English;
             //可以添加多项语言，但对应的，要添加Nuget包。比如：IronOcr.Languages.ChineseSimplified
             ironTeseract.AddSecondaryLanguage(OcrLanguage.ChineseSimplified);
+            ironTeseract.AddSecondaryLanguage(OcrLanguage.Khmer);
+            ironTeseract.AddSecondaryLanguage(OcrLanguage.Lao);
+            ironTeseract.AddSecondaryLanguage(OcrLanguage.Myanmar);
             using (var input = new OcrInput(fileName))
             {
-                var ocrResult = ironTeseract.Read(input);
+                var ocrResult =await ironTeseract.ReadAsync(input);
+                var filePath = @"C:\Users\y24914\Desktop\新建文本文档.txt";
+                if (File.Exists(filePath))
+                {
+                   File.Delete(filePath);
+                }
+                File.AppendAllText(filePath, ocrResult.Text, Encoding.UTF8);
                 return ocrResult.Text;
             }
         }
